3e71a9abda330b7ead4ec38b170d730482dc1ca9
lucpapo <papo@adn.com.br>
2017-09-25 10:08:14 -0300

    Code Review - add catch in promise - add shelf in book Search - remove state in changeShelf - remove update state direct

--- src/Component/Search.js
+++ src/Component/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import { Debounce } from 'react-throttle';
+import sortBy from 'sort-by'
 import * as BooksAPI from '../BooksAPI'
 import Book from './Book'
 
@@ -8,7 +9,19 @@ class Search extends React.Component {
 
   state = {
     query: '',
-    books: []
+    books: [],
+    booksSelected: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((booksSelected) => {
+      this.setState({ booksSelected })
+    })
+  }
+
+  getItemBookCase = (book) => {
+    var itemBookCase = this.state.booksSelected.filter((item) => item.id === book.id);
+    return (itemBookCase[0] ? itemBookCase[0].shelf : "none");
   }
 
   onSearchBook = (query) => {
@@ -16,16 +29,19 @@ class Search extends React.Component {
       this.setState({ books: [] });
     else
       BooksAPI.search(query.trim(), 20).then((books) => {
-
-        if (!books || books.error)
-          this.setState({ books: [] })
-        else
-          this.setState({ books })
+        this.setState({ books: books.sort(sortBy('title')) });
+      }).catch(() => {
+        this.setState({ books: [] });
       });
   }
 
-  render() {
+  updateBooks = (book) => {
+    this.setState((prevState) => ({
+      books: prevState.books.filter((c) => c.id !== book.id).concat(book).sort(sortBy('title'))
+    }))
+  }
 
+  render() {
     return (
       <div className="search-books">
         <div className="search-books-bar">
@@ -40,7 +56,7 @@ class Search extends React.Component {
         <div className="search-books-results">
           <ol className="books-grid">
             {this.state.books.map((book) => (
-              <Book book={book} key={book.id} ></Book>
+              <Book book={book} key={book.id} shelf={this.getItemBookCase(book)} onUpdateBooks={this.updateBooks}></Book>
             ))}
           </ol>
         </div>
